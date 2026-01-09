2832fcdc6a094595abbb6d769c2ff48e5502f802
Oluwapelumi Adeoye <davidadeoye22@gmail.com>
2026-01-09 12:11:18 +0100

    Replaced the search for a directory a file first if logic

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ LOGS_DIR="$ROOT_DIRECTORY/logs"
 BACKUPS_DIR="$ROOT_DIRECTORY/backups"
 LOG_FILE="$LOGS_DIR/deploy.log"
 
-if [ ! -d "$ROOT_DIRECTORY" ]; then
+if [ ! -f "$ROOT_DIRECTORY" ]; then
         echo "The root directory $ROOT_DIRECTORY does not exist..." >&2
         exit 1
 fi
